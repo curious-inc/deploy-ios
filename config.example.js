f3315cea516a309c9b023a7632975529f971f3bf
Kendrick Taylor <kendrick@suoiruc.com>
2015-01-04 18:42:58 -0800

    update example config

--- config.example.js
+++ config.example.js
@@ -1,12 +1,31 @@
 module.exports = {
     host: "install.example.com",
     builds: {
+        "unsecure": { 
+            secure: false,
+            bundle_name: "unsecure",
+            bundle_identifier: "com.example.unsecure" 
+        },
+        "secure" : { 
+            secure: true,
+            bundle_name: "secure",
+            bundle_identifier: "com.example.secure" 
+        },
+        "secure_unsecure_current": { 
+            secure: true,
+            secure_current: false,
+            bundle_name: "secure_unsecure_current",
+            bundle_identifier: "com.example.secure_unsecure_current" 
+        },
         "development" : { 
+            secure: true,
             bundle_name: "Foo (Development)",
             bundle_identifier: "com.example.development.Foo" 
         },
         "production": { 
             default: true,
+            secure: true,
+            secure_current: false,
             bundle_name: "Foo",
             bundle_identifier: "com.example.production.Foo" 
         }
@@ -17,7 +36,6 @@ module.exports = {
         ca_bundle: "./keys/example.cabundle"
     },
     auth: {
-        secure: true,
         username: "admin",
         password: "admin"
     },
